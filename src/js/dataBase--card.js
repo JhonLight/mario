d13fbed2093e9aa4500e8c7b1935c8c9f2f129c3
Jhon Light <jhony.mluz@gmail.com>
2022-09-04 20:44:57 -0300

    Base array dos personagens

---
+++ src/js/dataBase--card.js
@@ -0,0 +1,22 @@
+const personagens = [
+  {
+    id: "0",
+    nome: "boo",
+    img: "../img/Boo.png",
+  },
+  {
+    id: "1",
+    nome: "Luigui",
+    img: "../img/Luigi.png",
+  },
+  {
+    id: "2",
+    nome: "daisy",
+    img: "../img/Daisy.png",
+  },
+  {
+    id: "3",
+    nome: "coin",
+    img: "../img/Coin.png",
+  },
+];
