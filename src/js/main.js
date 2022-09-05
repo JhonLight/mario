b39795239953c0a390329c14fd51d350ead7072f
Jhon Light <jhony.mluz@gmail.com>
2022-09-04 21:05:53 -0300

    Criando cartas com js

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,16 @@ const main = document.getElementById('main')
 criaCartas();
 
 function criaCartas() {
-  for (let i = 0; i < 8; i++) {
-    let carta = document.createElement("img");
-    carta.src = "./src/img/QuestionBlock.png";
 
-    main.appendChild(carta)
+  for (let i = 0; i < personagens.length; i++) {
+    let cartaA = document.createElement("img");
+    cartaA.src = "./src/img/QuestionBlock.png";
+    let cartaB = document.createElement("img");
+    cartaB.src = "./src/img/QuestionBlock.png";
+
+
+    main.appendChild(cartaA)
+    main.appendChild(cartaB)
   }
 }
+
